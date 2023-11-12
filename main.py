dfa5436b7da9744141be419d575fbad3e689cedc
Cody Stiffler <codystiffleraz@gmail.com>
2023-11-12 15:54:32 -0700

    added SMS notifications

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
-from api_key import api_key
+from api_key import api_key, account_sid, auth_token, phone_number
+from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
+
+# "lat": 33.4484367,
+#     "lon": -112.074141,
+#     "appid": api_key
+
 weather_params = {
-    "lat": 33.4484367,
-    "lon": -112.074141,
+    "lat": 27.488510,
+    "lon": -99.507591,
     "appid": api_key
 }
 
@@ -20,7 +26,15 @@ for hour_data in weather_slice:
     condition_code = hour_data["weather"][0]["id"]
     if int(condition_code) < 700:
         will_rain = True
+    print(condition_code)    
         
 if will_rain:
-    print("Bring an umbrella")
-print(weather_data["list"][0]["weather"][0]["id"])
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+            body= "It's going to rain to day. Remember to bring an umbrella.",
+            from_= "+18447800396", 
+            to=phone_number
+    )
+    print(message.status)
+
+
